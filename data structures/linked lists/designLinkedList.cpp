330a3b0403338b6f8d33546ee506034c5f690187
Ashraf Jaffar <ashrafjfr@Ashrafs-MacBook-Pro.local>
2024-03-08 00:03:58 +0800

    edited solution for design linked list

--- data structures/linked lists/designLinkedList.cpp
+++ data structures/linked lists/designLinkedList.cpp
@@ -1,3 +1,33 @@
+/*
+Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement the MyLinkedList class:
+
+MyLinkedList() Initializes the MyLinkedList object.
+int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+void addAtTail(int val) Append a node of value val as the last element of the linked list.
+void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+Input
+["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+[[], [1], [3], [1, 2], [1], [1], [1]]
+Output
+[null, null, null, null, 2, null, 3]
+
+Explanation
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+*/
+
 class DoubleListNode {
 public:
     int val_;
@@ -23,13 +53,14 @@ public:
     
     int get(int index) {
         DoubleListNode *curr = head->next;
-        for (int i = 0; i < index; i++) {
+        while (curr && index > 0) {
             curr = curr->next;
-            if (curr == nullptr) {
-                return -1;
-            }
+            index--;
         }
-        return curr->val_;
+        if (curr && curr != tail && index == 0) {
+            return curr->val_;
+        }
+        return -1;
     }
     
     void addAtHead(int val) {
@@ -50,33 +81,30 @@ public:
     
     void addAtIndex(int index, int val) {
         DoubleListNode *curr = head->next;
-        for (int i = 0; i < index; i++) {
+        while (curr && index > 0) {
             curr = curr->next;
-            if (curr == nullptr) {
-                if (curr->prev == tail) {
-                    addAtTail(val);
-                } else {
-                    return;
-                }
-            }
+            index--;
         }
-        DoubleListNode *newNode = new DoubleListNode(val);
-        newNode->prev = curr;
-        newNode->next = curr->next;
-        curr->next->prev = newNode;
-        curr->next = newNode;
+        if (curr && index == 0) {
+            DoubleListNode *newNode = new DoubleListNode(val);
+            newNode->prev = curr->prev;
+            newNode->next = curr;
+            curr->prev->next = newNode;
+            curr->prev  = newNode;
+        }
+        
     }
     
     void deleteAtIndex(int index) {
         DoubleListNode *curr = head->next;
-        for (int i = 0; i < index; i++) {
+        while (curr && index > 0) {
             curr = curr->next;
-            if (curr == nullptr) {
-                return;
-            }
+            index--;
         }
-        curr->prev->next = curr->next;
-        curr->next->prev = curr->prev;
+        if (curr && curr != tail && index == 0) {
+            curr->prev->next = curr->next;
+            curr->next->prev = curr->prev;
+        }
     }
 };
 
